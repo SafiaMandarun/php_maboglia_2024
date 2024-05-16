1f437c779cf97dce041084b38a5611b80cb82057
Safia <safiamandarun@gmail.com>
2024-05-16 18:03:33 +0200

    up

--- ES_PHP/lezione1/index.php
+++ ES_PHP/lezione1/index.php
@@ -2,7 +2,7 @@
 <?php
 echo "Hello php!";
 
-for ($i=1; $i <= 1000; $i++) { //le variabili in php sono dinamiche e le introduci con $
+for ($i=1; $i <= 10; $i++) { //le variabili in php sono dinamiche e le introduci con $
     echo "<li> Hello php! $i </li>";
 }
 ?>
